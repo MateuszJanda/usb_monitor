33c8f6078d63f552077a166c4b08296be42f2921
Mateusz Janda <mateusz.janda@gmail.com>
2022-08-12 21:09:32 +0200

    Passint struct pointer to wndproc

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::HashSet;
 
 use futures::executor::block_on;
 use windows::core::*;
@@ -8,64 +8,208 @@ use windows::Win32::{
     Foundation::*, Graphics::Gdi::ValidateRect, System::LibraryLoader::GetModuleHandleA,
     UI::WindowsAndMessaging::*,
 };
+use windows::Win32::System::SystemServices;
 
-
+use std::mem::transmute;
 
 fn main() -> Result<()> {
-    unsafe {
-        let instance = GetModuleHandleA(None)?;
-        debug_assert!(instance.0 != 0);
+    let mut usb_monitor = UsbMonitor::new();
 
-        // Not predefined (by MS) class name, so must be registered by RegisterClassA
-        let window_class = s!("usb_monitor_window");
+    usb_monitor.run()
+}
 
-        let wc = WNDCLASSA {
-            hInstance: instance,
-            lpszClassName: window_class,
 
-            style: CS_HREDRAW | CS_VREDRAW,
-            lpfnWndProc: Some(wndproc),
-            ..Default::default()
-        };
+struct UsbMonitor {
+    handle: HWND,
+    devices: HashSet<String>,
+    // count: u32,
+}
 
-        let atom = RegisterClassA(&wc);
-        debug_assert!(atom != 0);
-
-        CreateWindowExA(
-            WINDOW_EX_STYLE::default(),
-            window_class,
-            s!("This is a sample window"),
-            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            None,
-            None,
-            instance,
-            std::ptr::null(),
-        );
-
-        let mut message = MSG::default();
-
-        let future = get_all_usb_info();
-        let mmm = block_on(future);
-
-        // println!("main {}", mmm.len());
-
-        while GetMessageA(&mut message, HWND(0), 0, 0).into() {
-            DispatchMessageA(&message);
-        }
-
-        Ok(())
+impl UsbMonitor {
+    fn new() -> Self {
+        UsbMonitor { handle: HWND(0), devices: HashSet::new() }
     }
+
+    fn run(&mut self) -> Result<()> {
+        unsafe {
+            let instance = GetModuleHandleA(None)?;
+            debug_assert!(instance.0 != 0);
+
+            // Not predefined (by MS) class name, so must be registered by RegisterClassA
+            let window_class = s!("usb_monitor_window");
+
+            let wc = WNDCLASSA {
+                hInstance: instance,
+                lpszClassName: window_class,
+
+                style: CS_HREDRAW | CS_VREDRAW,
+                lpfnWndProc: Some(Self::wndproc),
+                ..Default::default()
+            };
+
+            let atom = RegisterClassA(&wc);
+            debug_assert!(atom != 0);
+
+            let handle = CreateWindowExA(
+                WINDOW_EX_STYLE::default(),
+                window_class,
+                s!("This is a sample window"),
+                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                None,
+                None,
+                instance,
+                self as *mut _ as _,
+            );
+
+
+            // debug_assert!(handle.0 != 0);
+            // debug_assert!(handle == self.handle);
+
+            let mut message = MSG::default();
+
+            // let future = get_all_usb_info();
+            // let mmm = block_on(future);
+
+            // println!("main {}", mmm.len());
+
+            while GetMessageA(&mut message, HWND(0), 0, 0).into() {
+                DispatchMessageA(&message);
+            }
+
+            Ok(())
+        }
+    }
+
+    extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        unsafe {
+            if message == WM_NCCREATE {
+                println!("WM_NCCREATE");
+                // let cs = lparam.0 as *const CREATESTRUCTA;
+                // let this = (*cs).lpCreateParams as *mut Self;
+                let cs: &CREATESTRUCTA = transmute(lparam);
+                let this = cs.lpCreateParams as *mut Self;
+
+                // if !this.is_null()
+                // {
+                    (*this).handle = window;
+
+                // }
+                // else
+                // {
+                //     println!("PROBLEM");
+                // }
+
+
+                SetWindowLongPtrA(window, GWLP_USERDATA, this as _);
+
+                // SetWindowLongPtrA(window, GWLP_USERDATA, cs.lpCreateParams as _);
+
+
+            } else {
+                // println!("ELSE");
+                let this = GetWindowLongPtrA(window, GWLP_USERDATA) as *mut Self;
+                // (*this).handle = window;
+
+                if !this.is_null() {
+                    // println!("TUTAJ");
+                    return (*this).message_handler(window, message, wparam, lparam);
+                }
+                else {
+                    // println!("IS NULL");
+                }
+            }
+
+            // println!("UPS something wrong");
+            DefWindowProcA(window, message, wparam, lparam)
+        }
+    }
+
+    fn message_handler(&mut self, window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        unsafe {
+            // self.handle = window;
+            match message as u32 {
+
+                WM_CREATE => {
+
+                    println!("WM_CREATE ");
+
+                    // let cs = lparam.0 as *const CREATESTRUCTA;
+                    // let this = (*cs).lpCreateParams as *mut Self;
+                    // (*this).handle = window;
+
+                    // let future = get_all_usb_info();
+                    // self.devices = block_on(future);
+
+                    println!("WM_CREATE {}", self.devices.len());
+
+                    // (*this).count += 1;
+
+                    LRESULT(0)
+                },
+
+                WM_PAINT => {
+                    println!("WM_PAINT");
+                    ValidateRect(self.handle, std::ptr::null());
+                    LRESULT(0)
+                },
+                WM_DESTROY => {
+                    println!("WM_DESTROY");
+                    PostQuitMessage(0);
+                    LRESULT(0)
+                },
+                WM_DEVICECHANGE => {
+
+
+
+                    if wparam.0 as u32 == SystemServices::DBT_DEVICEARRIVAL  {
+                            println!("DBT_DEVICEARRIVAL ");
+
+                            let future = get_all_usb_info();
+                            let nnnmmm = block_on(future);
+
+                            // let cs = lparam.0 as *const CREATESTRUCTA;
+                            // let this = (*cs).lpCreateParams as *mut Self;
+                            // (*this).handle = window;
+
+
+                            for device_id in nnnmmm {
+                                if !self.devices.contains(&device_id) {
+
+                                    // println!("New device {}: {}-{} [{}]", count, value.0, value.1, device_id);
+                                    println!("New device: {}", device_id);
+
+                                    // mmm.insert(device_id, value);
+                                    self.devices.insert(device_id);
+                                }
+                            }
+
+                            // count += 1;
+                        }
+                        // DBT_DEVICEREMOVECOMPLETE => {
+                        //     println!("DBT_DEVICEREMOVECOMPLETE");
+                        // }
+
+                    LRESULT(0)
+                },
+
+
+                // _ => DefWindowProcA(self.handle, message, wparam, lparam),
+                _ => DefWindowProcA(window, message, wparam, lparam),
+            }
+        }
+    }
+
 }
 
 // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/how-to-connect-to-a-usb-device--uwp-app-#finding-the-devicethe-basic-way
 
-async fn get_all_usb_info() -> BTreeMap<String, (u32, u32)>
+async fn get_all_usb_info() -> HashSet<String>
 {
-    let mut result = BTreeMap::new();
+    let mut result = HashSet::new();
 
 
     if let Ok(operation) = DeviceInformation::FindAllAsync() {
@@ -89,7 +233,8 @@ async fn get_all_usb_info() -> BTreeMap<String, (u32, u32)>
 
                     if device_id.to_string().contains("\\USB#") {
 
-                        result.insert(device_id.to_string(), (0,0));
+                        // result.insert(device_id.to_string(), (0,0));
+                        result.insert(device_id.to_string());
                     }
 
 
@@ -152,60 +297,3 @@ async fn usb_product_and_vendor(device_id:&HSTRING) -> Option<(u32, u32)>
     None
 }
 
-extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    unsafe {
-        let mut mmm = BTreeMap::new();
-        let mut count = 0;
-
-        match message as u32 {
-            WM_CREATE => {
-                let future = get_all_usb_info();
-                mmm = block_on(future);
-
-                println!("WM_CREATE {}", mmm.len());
-
-                LRESULT(0)
-            },
-
-            WM_PAINT => {
-                println!("WM_PAINT");
-                ValidateRect(window, std::ptr::null());
-                LRESULT(0)
-            },
-            WM_DESTROY => {
-                println!("WM_DESTROY");
-                PostQuitMessage(0);
-                LRESULT(0)
-            },
-            WM_DEVICECHANGE => {
-
-                match wparam {
-                    DBT_DEVICEARRIVAL => {
-                        println!("DBT_DEVICEARRIVAL");
-
-                        let future = get_all_usb_info();
-                        let nnnmmm = block_on(future);
-
-                        for (device_id, value) in nnnmmm {
-                            if !mmm.contains_key(&device_id) {
-
-                                println!("New device {}: {}-{} [{}]", count, value.0, value.1, device_id);
-
-                                mmm.insert(device_id, value);
-                            }
-                        }
-
-                        count += 1;
-                    },
-                    // DBT_DEVICEREMOVECOMPLETE => {
-                    //     println!("DBT_DEVICEREMOVECOMPLETE");
-                    // }
-                 _ => ()
-                }
-
-                LRESULT(0)
-            },
-            _ => DefWindowProcA(window, message, wparam, lparam),
-        }
-    }
-}
