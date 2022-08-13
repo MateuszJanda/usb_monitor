2593d6b7fd971d7a32a50b498fa0a13d205ce54b
Mateusz Janda <mateusz.janda@gmail.com>
2022-08-13 18:16:08 +0200

    Update docs and prints

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::collections::HashSet;
 use windows::core::*;
 use windows::Devices::Enumeration::DeviceInformation;
 use windows::Win32::{
-    Foundation::*, Graphics::Gdi::ValidateRect, System::LibraryLoader::GetModuleHandleA,
-    System::SystemServices, UI::WindowsAndMessaging::*,
+    Foundation::*, System::LibraryLoader::GetModuleHandleA, System::SystemServices,
+    UI::WindowsAndMessaging::*,
 };
 
 fn main() -> Result<()> {
@@ -14,9 +14,9 @@ fn main() -> Result<()> {
 }
 
 struct UsbMonitor {
-    /// Handler to current window
+    /// Handler to current window.
     handle: HWND,
-    /// Set of all newly plugged USB devices
+    /// Set of all newly plugged USB devices.
     devices: HashSet<String>,
 }
 
@@ -34,7 +34,7 @@ impl UsbMonitor {
             let instance = GetModuleHandleA(None)?;
             debug_assert!(instance.0 != 0);
 
-            // Not predefined Windows class name, so will be registered by RegisterClassA
+            // Not predefined Windows class name, so will be registered by RegisterClassA.
             let window_class = s!("usb_monitor_window");
 
             let wc = WNDCLASSA {
@@ -49,12 +49,12 @@ impl UsbMonitor {
             let atom = RegisterClassA(&wc);
             debug_assert!(atom != 0);
 
-            // Register window. lpparam is set as pointer to current UsbMonitor
+            // Register window. lpParam is set as pointer to current UsbMonitor.
             let handle = CreateWindowExA(
                 WINDOW_EX_STYLE::default(),
                 window_class,
                 s!("This is a sample window"),
-                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                WS_OVERLAPPEDWINDOW,
                 CW_USEDEFAULT,
                 CW_USEDEFAULT,
                 CW_USEDEFAULT,
@@ -78,17 +78,13 @@ impl UsbMonitor {
         }
     }
 
+    /// Handle all window messages, especially WM_DEVICECHANGE when device or piece of media has
+    /// been inserted and is now available. It search then for newly plugged USB devices.
     fn message_handler(&mut self, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
         unsafe {
             match message as u32 {
                 WM_CREATE => {
-                    println!("WM_CREATE, archived devices: {}", self.devices.len());
-                    LRESULT(0)
-                }
-
-                WM_PAINT => {
-                    println!("WM_PAINT");
-                    ValidateRect(self.handle, std::ptr::null());
+                    println!("WM_CREATE. Archived USB info: {}", self.devices.len());
                     LRESULT(0)
                 }
                 WM_DESTROY => {
@@ -97,20 +93,27 @@ impl UsbMonitor {
                     LRESULT(0)
                 }
                 WM_DEVICECHANGE => {
-                    if wparam.0 as u32 == SystemServices::DBT_DEVICEARRIVAL {
-                        println!("DBT_DEVICEARRIVAL ");
+                    match wparam.0 as u32 {
+                        SystemServices::DBT_DEVICEARRIVAL => {
+                            println!("DBT_DEVICEARRIVAL. New device plugged.");
 
-                        let future = get_all_usb_info();
-                        let new_devices = block_on(future).unwrap();
+                            let future = get_all_usb_info();
+                            let new_devices = block_on(future).unwrap();
 
-                        for device_id in new_devices {
-                            if !self.devices.contains(&device_id) {
-                                println!("New device: {}", device_id);
-                                self.devices.insert(device_id);
+                            for device_id in new_devices {
+                                if !self.devices.contains(&device_id) {
+                                    println!("New USB info: {}", device_id);
+                                    self.devices.insert(device_id);
+                                }
                             }
-                        }
-                    }
 
+                            println!("Archived USB info: {}", self.devices.len());
+                        }
+                        SystemServices::DBT_DEVICEREMOVECOMPLETE => {
+                            println!("DBT_DEVICEARRIVAL. Device unplugged.")
+                        }
+                        _ => (),
+                    }
                     LRESULT(0)
                 }
 
@@ -120,12 +123,14 @@ impl UsbMonitor {
     }
 }
 
-/// Basic function to processes messages (registered in RegisterClassA), that
-/// that pass all messages to UsbMonitor object (message_handler method).
+/// Basic function to processes messages (registered in RegisterClassA), that that pass all messages
+/// to UsbMonitor object (message_handler method).
 extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     unsafe {
+        // WM_NCCREATE is called even before WM_CREATE, so this is good place to read and save
+        // pointer to UsbMonitor object.
         if message == WM_NCCREATE {
-            println!("WM_NCCREATE");
+            println!("WM_NCCREATE. Saving pointer to UsbMonitor object.");
             let cs = lparam.0 as *const CREATESTRUCTA;
             let this = (*cs).lpCreateParams as *mut UsbMonitor;
 
@@ -147,8 +152,8 @@ extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: L
     }
 }
 
-/// Return raw information about USB devices (DeviceInformation.Id). Function
-/// use UWP (Universal Windows Platform) API.
+/// Iterate trough all devices and return raw information about USB devices (DeviceInformation.Id).
+/// Function use UWP (Universal Windows Platform) API.
 ///
 /// [Finding the device](https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/how-to-connect-to-a-usb-device--uwp-app-#finding-the-devicethe-basic-way)
 async fn get_all_usb_info() -> Result<HashSet<String>> {
